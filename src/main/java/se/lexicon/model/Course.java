da7ab3f92dae6c44d1b2b9410d65115327adc1ae
Gentrithotii <Gentrithoti@hotmail.com>
2025-04-10 09:23:43 +0200

    Added models

---
+++ src/main/java/se/lexicon/model/Course.java
@@ -0,0 +1,52 @@
+package se.lexicon.model;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public class Course {
+    private int id;
+    private String courseName;
+    private LocalDate startDate;
+    private int weekDuration;
+    private List<Student> students;
+
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public void setCourseName(String courseName) {
+        this.courseName = courseName;
+    }
+
+    public LocalDate getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(LocalDate startDate) {
+        this.startDate = startDate;
+    }
+
+    public int getWeekDuration() {
+        return weekDuration;
+    }
+
+    public void setWeekDuration(int weekDuration) {
+        this.weekDuration = weekDuration;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public List<Student> getStudents() {
+        return students;
+    }
+
+}
+
+
